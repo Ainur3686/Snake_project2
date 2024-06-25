021c04c6642cd3a2aab793833bd295c65b6b748c
Ainur Botagarova <botagarovaainura@gmail.com>
2024-06-25 18:27:57 -0500

    Step 2

--- snake.py
+++ snake.py
@@ -20,3 +20,29 @@ for row in table:
     for element in row:
         print(element, end = ' ')
     print('\n') 
+
+#Step 2. Write a movement function that gets the coordinates as a list and the direction keyword ('n','s','e','w') and adds to that list, 
+#The last point “moved” - will be added in that direction.
+
+""" Modify the existing list by appending a new coordinate in the specified direction """
+def change_table(table, direction):
+    y = table[-1][0]
+    x = table[-1][1]
+    if direction == 'e' and x < 9:
+      x += 1
+    elif direction == 'w' and x > 0:
+      x -= 1
+    elif direction == 'n' and y > 0:
+      y -= 1
+    elif direction == 's' and y < 9:
+      y += 1
+    else:
+      pass
+
+    table.append((y,x))
+
+table = [(0,0)]
+change_table(table,'e')
+change_table(table,'e')
+change_table(table,'n')
+print(table)
