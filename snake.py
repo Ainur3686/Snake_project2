b5942ea1f6e73e474f487296ef24064f824c78f3
Ainur Botagarova <botagarovaainura@gmail.com>
2024-06-25 19:59:51 -0500

    Step 3.

--- snake.py
+++ snake.py
@@ -22,11 +22,13 @@ for row in table:
     print('\n') 
 
 #Step 2. Write a movement function that gets the coordinates as a list and the direction keyword ('n','s','e','w') and adds to that list, 
-#The last point “moved” - will be added in that direction.
+#The last point “moved” - will be added in that direction. 
 
-""" Modify the existing list by appending a new coordinate in the specified direction """
+""" Modify the existing list by appending a new coordinate in the specified direction."""
 def change_table(table, direction):
+    
     y, x = table[-1]
+    
     if direction == 'e' and x < 9:
       x += 1
     elif direction == 'w' and x > 0:
@@ -36,12 +38,17 @@ def change_table(table, direction):
     elif direction == 's' and y < 9:
       y += 1
     else:
-       return False
-    
-    table.append((y,x))
+       return False 
+
+table.append((y,x))
 
 table = [(0,0)]
 change_table(table,'e')
 change_table(table,'e')
 change_table(table,'n')
 print(table)
+
+#Step 3. Start with the initial list of [(0,0),(0,1),(0,2)] coordinates. Write a while loop, ask the user for the movement direction,  
+# until the user stops it by writing ‘end’ and then draws the list as a map (as in step 1). 
+# This makes the snake move and grow (combination of tasks 1 and 2)
+
